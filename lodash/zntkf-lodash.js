ac505f7c68a5617268de8ec8159fb3823bef1f3e
1056 <1056439669@qq.com>
2022-01-12 15:12:35 +0800

    lodash创建

---
+++ lodash/zntkf-lodash.js
@@ -0,0 +1,1110 @@
+var zntkf = function () {
+
+
+
+
+  function chunk() {
+   
+  }
+
+
+  function compact() {
+   
+  }
+
+
+  function difference() {
+
+  }
+  function differenceBy() {
+
+  }
+  function differenceWith() {
+
+  }
+
+  function drop() {
+ 
+  }
+
+
+  function dropRight() {
+    
+  }
+
+
+
+
+  function dropRightWhile() {
+
+  }
+  function dropWhile() {
+
+  }
+  function fill() {
+
+  }
+  function findIndex() {
+
+  }
+  function indLastIndex() {
+
+  }
+  function findLastIndex() {
+
+  }
+
+
+
+  function flatten() {
+   
+  }
+
+
+  function flattenDeep() {
+  
+  }
+
+
+
+  function flattenDepth() {
+    
+  }
+
+
+  function fromPairs() {
+   
+  }
+
+
+
+  function head() {
+    
+  }
+
+
+
+  function indexOf() {
+   
+  }
+
+
+  function initial() {
+   
+  }
+
+
+  function intersection() {
+
+  }
+  function intersectionBy() {
+
+  }
+  function intersectionWith() {
+
+  }
+
+
+
+  function join() {
+  
+  }
+
+
+
+  function last() {
+   
+  }
+
+
+  function lastIndexOf() {
+    fo
+  }
+
+
+  function nth() {
+
+  }
+
+  function pull() {
+
+  }
+  function pullAll() {
+
+  }
+  function pullAllBy() {
+
+  }
+  function pullAllWith() {
+
+  }
+  function remove() {
+
+  }
+
+
+
+  function reverse() {
+    
+  }
+
+
+
+
+  function slice() {
+
+  }
+  function sortedIndex() {
+
+  }
+  function sortedIndexBy() {
+
+  }
+  function sortedIndexOf() {
+
+  }
+  function sortedLastIndex() {
+
+  }
+  function sortedLastIndexBy() {
+
+  }
+  function sortedLastIndexOf() {
+
+  }
+  function sortedUniq() {
+
+  }
+  function sortedUniqBy() {
+
+  }
+  function tail() {
+
+  }
+  function take() {
+
+  }
+  function takeRight() {
+
+  }
+  function takeRightWhile() {
+
+  }
+  function takeWhile() {
+
+  }
+  function union() {
+
+  }
+  function unionBy() {
+
+  }
+  function unionWith() {
+
+  }
+  function uniqWith() {
+
+  }
+  function uniq() {
+
+  }
+  function uniqBy() {
+
+  }
+  function unzip() {
+
+  }
+  function unzipWith() {
+
+  }
+
+
+  function without() {
+   
+  }
+
+
+  function xor() {
+
+  }
+  function xorBy() {
+
+  }
+  function xorWith() {
+
+  }
+  function zipObject() {
+
+  }
+  function zipObjectDeep() {
+
+  }
+  function zipWith() {
+
+  }
+  function zip() {
+
+  }
+  function countBy() {
+
+  }
+  function every() {
+
+  }
+  function filter() {
+
+  }
+  function find() {
+
+  }
+  function findLast() {
+
+  }
+  function flatMap() {
+
+  }
+  function flatMapDeep() {
+
+  }
+  function flatMapDepth() {
+
+  }
+  function forEach() {
+
+  }
+  function forEachRight() {
+
+  }
+  function groupBy() {
+
+  }
+  function includes() {
+
+  }
+  function invokeMap() {
+
+  }
+  function keyBy() {
+
+  }
+  function map() {
+
+  }
+  function orderBy() {
+
+  }
+  function partition() {
+
+  }
+  function reduce() {
+
+  }
+  function reduceRight() {
+
+  }
+  function reject() {
+
+  }
+  function sample() {
+
+  }
+  function sampleSize() {
+
+  }
+  function shuffle() {
+
+  }
+  function size() {
+
+  }
+  function some() {
+
+  }
+  function sortBy() {
+
+  }
+  function defer() {
+
+  }
+  function delay() {
+
+  }
+  function castArray() {
+
+  }
+  function conformsTo() {
+
+  }
+  function eq() {
+
+  }
+  function gt() {
+
+  }
+  function gte() {
+
+  }
+  function isArguments() {
+
+  }
+  function isArray() {
+
+  }
+  function isArrayBuffer() {
+
+  }
+
+  function isArrayLike() {
+
+  }
+  function isArrayLikeObject() {
+
+  }
+  function isBoolean() {
+
+  }
+  function isBoolean() {
+
+  }
+  function isDate() {
+
+  }
+  function isElement() {
+
+  }
+
+  function isEmpty() {
+
+  }
+  function isEqual() {
+
+  }
+  function isEqualWith() {
+
+  }
+  function isError() {
+
+  }
+  function isFunction() {
+
+  }
+  function isInteger() {
+
+  }
+  function isLength() {
+
+  }
+  function isMap() {
+
+  }
+  function isMatch() {
+
+  }
+  function isMatchWith() {
+
+  }
+  function isNaN() {
+
+  }
+
+  function isNil() {
+
+  }
+  function isNative() {
+
+  }
+  function isNull() {
+
+  }
+  function isNumber() {
+
+  }
+  function isObject() {
+
+  }
+  function isObjectLike() {
+
+  }
+  function isPlainObject() {
+
+  }
+
+  function isRegExp() {
+
+  }
+  function isSafeInteger() {
+
+  }
+  function isSet() {
+
+  }
+  function isString() {
+
+  }
+  function isSymbol() {
+
+  }
+  function isTypedArray() {
+
+  }
+  function isUndefined() {
+
+  }
+  function isWeakMap() {
+
+  }
+  function isWeakSet() {
+
+  }
+  function lt() {
+
+  }
+  function lte() {
+
+  }
+  function toArray() {
+
+  }
+  function toFinite() {
+
+  }
+
+  function toInteger() {
+
+  }
+  function toLength() {
+
+  }
+  function toNumber() {
+
+  }
+  function assign() {
+
+  }
+  function toSafeInteger() {
+
+  }
+  function add() {
+
+  }
+
+  function ceil() {
+
+  }
+  function divide() {
+
+  }
+  function floor() {
+
+  }
+  function max() {
+
+  }
+  function maxBy() {
+
+  }
+  function mean() {
+
+  }
+  function meanBy() {
+
+  }
+
+  function min() {
+
+  }
+  function minBy() {
+
+  }
+  function multiply() {
+
+  }
+  function round() {
+
+  }
+  function subtract() {
+
+  }
+
+
+
+  function sumBy() {
+
+  }
+  function sum() {
+
+  }
+  function clamp() {
+
+  }
+  function inRange() {
+
+  }
+  function random() {
+
+  }
+  function assignIn() {
+
+  }
+  function at() {
+
+  }
+  function defaults() {
+
+  }
+  function defaultsDeep() {
+
+  }
+  function findKey() {
+
+  }
+  function findLastKey() {
+
+  }
+  function forIn() {
+
+  }
+  function forInRight() {
+
+  }
+  function forOwn() {
+
+  }
+  function forOwnRight() {
+
+  }
+  function functions() {
+
+  }
+  function functionsIn() {
+
+  }
+  function get() {
+
+  }
+  function has() {
+
+  }
+  function hasIn() {
+
+  }
+  function invert() {
+
+  }
+  function invertBy() {
+
+  }
+  function keys() {
+
+  }
+  function keysIn() {
+
+  }
+  function mapKeys() {
+
+  }
+  function mapValues() {
+
+  }
+  function merge() {
+
+  }
+
+  function mergeWith() {
+
+  }
+  function omit() {
+
+  }
+  function omitBy() {
+
+  }
+
+  function pick() {
+
+  }
+  function pickBy() {
+
+  }
+  function result() {
+
+  }
+  function set() {
+
+  }
+  function setWith() {
+
+  }
+  function toPairs() {
+
+  }
+  function toPairsIn() {
+
+  }
+  function transform() {
+
+  }
+  function unset() {
+
+  }
+  function update() {
+
+  }
+  function updateWith() {
+
+  }
+  function values() {
+
+  }
+  function valuesIn() {
+
+  }
+  function camelCase() {
+
+  }
+
+  function capitalize() {
+
+  }
+  function deburr() {
+
+  }
+  function endsWith() {
+
+  }
+  function deburr() {
+
+  }
+  function escape() {
+
+  }
+  function escapeRegExp() {
+
+  }
+  function kebabCase() {
+
+  }
+  function lowerCase() {
+
+  }
+  function pad() {
+
+  }
+  function padEnd() {
+
+  }
+  function padStart() {
+
+  }
+
+  function repeat() {
+
+  }
+  function replace() {
+
+  }
+  function snakeCase() {
+
+  }
+  function split() {
+
+  }
+  function startCase() {
+
+  }
+  function startsWith() {
+
+  }
+  function toLower() {
+
+  }
+  function toUpper() {
+
+  }
+  function trim() {
+
+  }
+  function trimEnd() {
+
+  }
+  function trimStart() {
+
+  }
+  function truncate() {
+
+  }
+  function unescape() {
+
+  }
+  function upperCase() {
+
+  }
+  function upperFirst() {
+
+  }
+  function words() {
+
+  }
+  function bindAll() {
+
+  }
+  function defaultTo() {
+
+  }
+  function range() {
+
+  }
+  function rangeRight() {
+
+  }
+  function mixin() {
+
+  }
+  function times() {
+
+  }
+  function toPath() {
+
+  }
+  function uniqueId() {
+
+  }
+  function cloneDeep() {
+
+  }
+  function uniqueId() {
+
+  }
+  function identity() {
+
+  }
+  function concat() {
+
+  }
+  function pullAt() {
+
+  }
+  function matches() {
+
+  }
+  function property() {
+
+  }
+  function ary() {
+
+  }
+  function unary() {
+
+  }
+  function negate() {
+
+  }
+  function once() {
+
+  }
+  function spread() {
+
+  }
+  function curry() {
+
+  }
+  function memoize() {
+
+  }
+  function flip() {
+
+  }
+  function conforms() {
+
+  }
+  function constant() {
+
+  }
+  function flow() {
+
+  }
+  function method() {
+
+  }
+  function methodOf() {
+
+  }
+  function nthArg() {
+
+  }
+  function propertyOf() {
+
+  }
+  function parseJson() {
+
+  }
+  function stringifyJson() {
+
+  }
+
+
+
+
+
+
+  // return {
+  //   chunk: chunk,
+  //   compact: compact,
+  //   difference: difference,
+  //   differenceBy: differenceBy,
+  //   differenceWith: differenceWith,
+  //   drop: drop,
+  //   dropRight: dropRight,
+  //   dropRightWhile: dropRightWhile,
+  //   dropWhile: dropWhile,
+  //   fill: fill,
+  //   findIndex: findIndex,
+  //   findLastIndex: findLastIndex,
+  //   indLastIndex: indLastIndex,
+  //   flatten: flatten,
+  //   flattenDeep: flattenDeep,
+  //   flattenDepth: flattenDepth,
+  //   fromPairs: fromPairs,
+  //   head: head,
+  //   indexOf: indexOf,
+  //   initial: initial,
+  //   intersection: intersection,
+  //   intersectionBy: intersectionBy,
+  //   intersectionWith: intersectionWith,
+  //   join: join,
+  //   last: last,
+  //   lastIndexOf: lastIndexOf,
+  //   nth: nth,
+  //   pull: pull,
+  //   pullAll: pullAll,
+  //   pullAllBy: pullAllBy,
+  //   pullAllWith: pullAllWith,
+  //   remove: remove,
+  //   reverse: reverse,
+  //   slice: slice,
+  //   sortedIndex: sortedIndex,
+  //   sortedIndexBy: sortedIndexBy,
+  //   sortedIndexOf: sortedIndexOf,
+  //   sortedLastIndex: sortedLastIndex,
+  //   sortedLastIndexBy: sortedLastIndexBy,
+  //   sortedLastIndexOf: sortedLastIndexOf,
+  //   sortedUniq: sortedUniq,
+  //   sortedUniqBy: sortedUniqBy,
+  //   tail: tail,
+  //   take: take,
+  //   takeRight: takeRight,
+  //   takeRightWhile: takeRightWhile,
+  //   takeWhile: takeWhile,
+  //   union: union,
+  //   unionBy: unionBy,
+  //   unionWith: unionWith,
+  //   uniq: uniq,
+  //   uniqBy: uniqBy,
+  //   uniqWith: uniqWith,
+  //   unzip: unzip,
+  //   unzipWith: unzipWith,
+  //   without: without,
+  //   xor: xor,
+  //   xorBy: xorBy,
+  //   xorWith: xorWith,
+  //   zipObject: zipObject,
+  //   zipObjectDeep: zipObjectDeep,
+  //   zipWith: zipWith,
+  //   zip: zip,
+  //   countBy: countBy,
+  //   every: every,
+  //   filter: filter,
+  //   find: find,
+  //   findLast: findLast,
+  //   flatMap: flatMap,
+  //   flatMapDeep: flatMapDeep,
+  //   flatMapDepth: flatMapDepth,
+  //   forEach: forEach,
+  //   forEachRight: forEachRight,
+  //   groupBy: groupBy,
+  //   includes: includes,
+  //   invokeMap: invokeMap,
+  //   keyBy: keyBy,
+  //   map: map,
+  //   orderBy: orderBy,
+  //   partition: partition,
+  //   reduce: reduce,
+  //   reduceRight: reduceRight,
+  //   reject: reject,
+  //   sample: sample,
+  //   sampleSize: sampleSize,
+  //   shuffle: shuffle,
+  //   size: size,
+  //   some: some,
+  //   sortBy: sortBy,
+  //   defer: defer,
+  //   delay: delay,
+  //   castArray: castArray,
+  //   conformsTo: conformsTo,
+  //   eq: eq,
+  //   gt: gt,
+  //   gte: gte,
+  //   isArguments: isArguments,
+  //   isArray: isArray,
+  //   isArrayBuffer: isArrayBuffer,
+  //   isArrayLike: isArrayLike,
+  //   isArrayLikeObject: isArrayLikeObject,
+  //   isBoolean: isBoolean,
+  //   isDate: isDate,
+  //   isElement: isElement,
+  //   isEmpty: isEmpty,
+  //   isEqual: isEqual,
+  //   isEqualWith: isEqualWith,
+  //   isError: isError,
+  //   isFinite: isFinite,
+  //   isFunction: isFunction,
+  //   isInteger: isInteger,
+  //   isLength: isLength,
+  //   isMap: isMap,
+  //   isMatch: isMatch,
+  //   isMatchWith: isMatchWith,
+  //   isNaN: isNaN,
+  //   isNative: isNative,
+  //   isNil: isNil,
+  //   isNull: isNull,
+  //   isNumber: isNumber,
+  //   isObject: isObject,
+  //   isObjectLike: isObjectLike,
+  //   isPlainObject: isPlainObject,
+  //   isRegExp: isRegExp,
+  //   isSafeInteger: isSafeInteger,
+  //   isSet: isSet,
+  //   isString: isString,
+  //   isSymbol: isSymbol,
+  //   isTypedArray: isTypedArray,
+  //   isUndefined: isUndefined,
+  //   isWeakMap: isWeakMap,
+  //   isWeakSet: isWeakSet,
+  //   lt: lt,
+  //   lte: lte,
+  //   toArray: toArray,
+  //   toFinite: toFinite,
+  //   toInteger: toInteger,
+  //   toLength: toLength,
+  //   toNumber: toNumber,
+  //   assign: assign,
+  //   toSafeInteger: toSafeInteger,
+  //   add: add,
+  //   ceil: ceil,
+  //   divide: divide,
+  //   floor: floor,
+  //   max: max,
+  //   maxBy: maxBy,
+  //   mean: mean,
+  //   meanBy: meanBy,
+  //   min: meanBy,
+  //   minBy: minBy,
+  //   multiply: multiply,
+  //   round: round,
+  //   subtract: subtract,
+  //   sumBy: sumBy,
+  //   sum: sum,
+  //   clamp: clamp,
+  //   inRange: inRange,
+  //   random: random,
+  //   assignIn: assignIn,
+  //   at: at,
+  //   defaults: defaults,
+  //   defaultsDeep: defaultsDeep,
+  //   findKey: findKey,
+  //   findLastKey: findLastKey,
+  //   forIn: forIn,
+  //   forInRight: forInRight,
+  //   forOwn: forOwn,
+  //   forOwnRight: forOwnRight,
+  //   functions: functions,
+  //   functionsIn: functionsIn,
+  //   get: get,
+  //   has: has,
+  //   hasIn: hasIn,
+  //   invert: invert,
+  //   invertBy: invertBy,
+  //   keys: keys,
+  //   keysIn: keysIn,
+  //   mapKeys: mapKeys,
+  //   mapValues: mapValues,
+  //   merge: merge,
+  //   mergeWith: mergeWith,
+  //   omit: omit,
+  //   omitBy: omitBy,
+  //   pick: pick,
+  //   pickBy: pickBy,
+  //   result: result,
+  //   set: set,
+  //   setWith: setWith,
+  //   toPairs: toPairs,
+  //   toPairsIn: toPairsIn,
+  //   transform: transform,
+  //   unset: unset,
+  //   update: update,
+  //   updateWith: updateWith,
+  //   values: values,
+  //   valuesIn: valuesIn,
+  //   camelCase: camelCase,
+  //   capitalize: capitalize,
+  //   deburr: deburr,
+  //   endsWith: endsWith,
+  //   escape: escape,
+  //   escapeRegExp: escapeRegExp,
+  //   kebabCase: kebabCase,
+  //   lowerCase: lowerCase,
+  //   lowerFirst: lowerCase,
+  //   pad: pad,
+  //   padEnd: padEnd,
+  //   padStart: padStart,
+  //   parseInt: parseInt,
+  //   repeat: repeat,
+  //   replace: replace,
+  //   snakeCase: snakeCase,
+  //   split: split,
+  //   startCase: startCase,
+  //   startsWith: startsWith,
+  //   toLower: toLower,
+  //   toUpper: toUpper,
+  //   trim: trim,
+  //   trimEnd: trimEnd,
+  //   trimStart: trimStart,
+  //   truncate: truncate,
+  //   unescape: unescape,
+  //   upperCase: upperCase,
+  //   upperFirst: upperFirst,
+  //   words: words,
+  //   bindAll: bindAll,
+  //   defaultTo: defaultTo,
+  //   range: range,
+  //   rangeRight: rangeRight,
+  //   mixin: mixin,
+  //   times: times,
+  //   toPath: toPath,
+  //   uniqueId: uniqueId,
+  //   cloneDeep: cloneDeep,
+  //   identity: identity,
+  //   concat: concat,
+  //   pullAt: pullAt,
+  //   matches: matches,
+  //   property: property,
+  //   ary: ary,
+  //   unary: unary,
+  //   negate: negate,
+  //   once: once,
+  //   spread: spread,
+  //   curry: curry,
+  //   memoize: memoize,
+  //   flip: flip,
+  //   conforms: conforms,
+  //   constant: constant,
+  //   flow: flow,
+  //   method: method,
+  //   methodOf: methodOf,
+  //   nthArg: nthArg,
+  //   propertyOf: propertyOf,
+  //   parseJson: parseJson,
+  //   stringifyJson: stringifyJson,
+  //}
+  //()
+}
